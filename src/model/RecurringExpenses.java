171f3558fca7c28f50cf07ba98021fa9b6596906
Isaac Buitrago <mightyking7@gmail.com>
2017-10-20 01:59:03 -0500

    Removed external folder. Created file based system for storing user data.

---
+++ src/model/RecurringExpenses.java
@@ -0,0 +1,54 @@
+package model;
+
+import java.util.ArrayList;
+
+
+/**
+ * This class is responsible for tracking a user's living Expenses
+ * on a monthly basis.
+ * @author Isaac Buitrago
+ *
+ */
+
+public class RecurringExpenses{
+	
+	/**
+	 * Living expenses include Housing, Auto expenses, and General Insurance payments
+	 */
+	private ArrayList<Expense> livingExpenses;
+	
+	public RecurringExpenses() {
+
+		livingExpenses = new ArrayList<Expense>();
+	}
+	
+	/**
+	 *@return ArrayList of expenses for the month 
+	 */
+	public ArrayList<Expense> getExpenses(){
+		
+		return this.livingExpenses;
+	}
+	
+	/**
+	 * 
+	 * @param expense made during the month
+	 */
+	public void addExpense(Expense expense) {
+		
+		this.livingExpenses.add(expense);
+	}
+	
+	/**
+	 * This function removes an expense from the list.
+	 * @param expense to be deleted from the list
+	 */
+	
+	public void removeExpense(Expense expense) {
+		
+		this.livingExpenses.remove(expense);
+		
+	}
+	
+
+}
