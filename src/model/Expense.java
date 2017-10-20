6c14885c8f6da8ed846f3c4677e20332a0353640
Isaac Buitrago <mightyking7@gmail.com>
2017-10-20 02:01:04 -0500

    Created FixedExpenses class, added deleteExpense method to RecurringExpenses, and added author tag to Expense abstract class.

--- src/model/Expense.java
+++ src/model/Expense.java
@@ -1,5 +1,49 @@
 package model;
 
+/**
+ * 
+ * @author Isaac Buitrago
+ *
+ */
+
 public abstract class Expense {
+	
+	private double amount;			// The amount of the Expense
+	
+	private Date transactionDate;	// The Date the Expense was made
+	
+	public Expense(double amount, Date date) {
+		
+		if(amount >= 0 ) {
+			
+			this.amount = amount;
+		}
+		
+		this.transactionDate = date;
+	}
+	
+	public double getAmmount() 
+	{
+		return this.amount;
+	}
+	
+	public void setAmmount(double amount)
+	{
+		if(amount >= 0)
+		{
+			this.amount = amount;
+		}
+		
+	}
+	
+	public Date getDate() 
+	{
+		return this.transactionDate;
+	}
+	
+	public void setDate(Date date) 
+	{
+		this.transactionDate = date;
+	}	
 
 }
---
+++ src/model/FixedExpenses.java
@@ -0,0 +1,15 @@
+package model;
+
+/**
+ * Fixed Expenses occur periodically at a specific date.
+ * The user's bills are a Fixed Expense since they occur
+ * on a certain date each month and do not change as frequent as
+ * living expenses, like gas and food.
+ * @author Isaac Buitrago
+ *
+ */
+public class FixedExpenses {
+	
+	
+
+}
