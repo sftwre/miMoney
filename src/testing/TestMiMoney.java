8745b921e9baf1b3a301022331b7e96585fff101
Manuel De Aguinaga <isra_de@outlook.com>
2017-10-23 22:20:50 -0500

    TestMiMoney package has being added, we can use it to test our objects

---
+++ src/testing/TestMiMoney.java
@@ -0,0 +1,20 @@
+package testing;
+
+/**
+ * Lets use it to test our own objects
+ */
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import model.Goals;
+import model.Budget;
+import model.Date;
+import model.Expense;
+import model.FixedExpenses;
+import model.Income;
+import model.miMoneyTester;
+import model.RecurringExpenses;
+
+public class TestMiMoney {
+	
+}
