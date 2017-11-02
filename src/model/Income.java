3c7a252b0203716bb39ac880bdb45de79b36c1bd
Raul Cecena <cecena.r55@gmail.com>
2017-11-02 09:38:13 -0500

    Income comm

--- src/model/Income.java
+++ src/model/Income.java
@@ -1,5 +1,41 @@
+/**
+ * 
+ * @author Raul Cecena
+ *
+ */
+
 package model;
+
+import java.util.*;
 
 public class Income {
 
-}
+	private String title;
+	private double pay;
+
+	public void userProfile(String t, double p) {
+
+	
+		this.title = t;
+		this.pay = p;
+
+	}// userProfile
+
+	
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+			this.title = title;
+	}
+
+	public double getPay() {
+		return this.pay;
+	}
+
+	// user incomes stores how much they makes and job title.
+	//
+
+}// End public Income
