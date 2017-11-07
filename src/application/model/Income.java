dd16038f84edef028765569d51c917bcc4273249
Isaac Buitrago <mightyking7@gmail.com>
2017-11-07 10:50:33 -0600

    fields set to private, getters and setters for private fields created

--- src/application/model/Income.java
+++ src/application/model/Income.java
@@ -2,15 +2,35 @@ package application.model;
 
 public class Income {
 	
-	String jobTitle;
+	private String jobTitle;
 	
-	double wage;
+	private double wage;
 	
 	
 	public Income(String jobTitle, double wage)
 	{
 		this.jobTitle = jobTitle;
 		this.wage = wage;
+	}
+	
+	public void setjobTitle(String jobTitle)
+	{
+		this.jobTitle = jobTitle;
+	}
+	
+	public String getjobTitle()
+	{
+		return (this.jobTitle);
+	}
+	
+	public void setWage(double wage)
+	{
+		this.wage = wage;
+	}
+	
+	public double getWage()
+	{
+		return(this.wage);
 	}
 
 }
