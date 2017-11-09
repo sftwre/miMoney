1d3668177442751137aa531c91c74373f29f77b6
kel <kel@172.24.64.255>
2017-11-09 08:35:28 -0600

    tutorial controls

---
+++ src/application/controller/TutorialController.java
@@ -0,0 +1,22 @@
+package application.controller;
+import java.awt.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TutorialController {
+
+    @FXML
+    private Button nextSceneButton;
+
+    @FXML
+    void nextScene(ActionEvent event) {
+    	
+
+    }
+    @FXML
+    void getStarted(ActionEvent event) {
+
+    }
+
+}
