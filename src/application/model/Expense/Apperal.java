67977b9a9e2412be67a0080d8454630d044e3043
Isaac Buitrago <mightyking7@gmail.com>
2017-11-12 16:51:27 -0600

    Created variable Expenses of type Expense

---
+++ src/application/model/Expense/Apperal.java
@@ -0,0 +1,17 @@
+package application.model.Expense;
+
+import application.model.Date;
+
+/**
+ * 
+ * @author Isaac Buitrago
+ *
+ */
+public class Apperal extends Expense implements VariableExpense {
+
+	public Apperal(double amount, Date date, String item) {
+		super(amount, date, item);
+		
+	}
+
+}
---
+++ src/application/model/Expense/Entertainment.java
@@ -0,0 +1,17 @@
+package application.model.Expense;
+
+import application.model.Date;
+
+/**
+ * 
+ * @author Isaac Buitrago
+ *
+ */
+public class Entertainment extends Expense implements VariableExpense {
+
+	public Entertainment(double amount, Date date, String item) {
+		super(amount, date, item);
+		
+	}
+
+}
