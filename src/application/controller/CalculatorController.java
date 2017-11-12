08fb15b58ca5f4511196deab01f43d0626ed91db
Isaac Buitrago <mightyking7@gmail.com>
2017-11-12 15:19:29 -0600

    Cleaned files related to the SavingsCalculator.

--- src/application/controller/CalculatorController.java
+++ src/application/controller/CalculatorController.java
@@ -1,12 +1,18 @@
-package calculator.controller;
+package application.controller;
 
-import calculator.main.CalculatorMain;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
+//TODO Please file rename to something more appropriate like SavingsCalculatorController
+
+/**
+ * 
+ * @author Sam Dash
+ *
+ */
 public class CalculatorController implements EventHandler<ActionEvent> {
 
 	@Override
@@ -14,13 +20,20 @@ public class CalculatorController implements EventHandler<ActionEvent> {
 		// print a message to the console (debugging step, to ensure everything is connected)
 		System.out.println("clicked!");
 		
+		/*
+		 * Is this necessary? The controller should process the data the User entered to
+		 * produce output relative to the savings goal.
+		 
 		try {
 			// change over to a second view
+			
+			
 			Parent root = FXMLLoader.load(getClass().getResource("/CalcView.fxml"));   // Load the FXML
 			CalculatorMain.stage.setScene(new Scene(root, 500, 575));							   // Add the scene to the stage
 			CalculatorMain.stage.show();														   // Show the stage to the user
 		}catch(Exception e) {
 			e.printStackTrace(); // TODO: app should do something more productive if errors occur...
 		}
+		 */	
 	}
 }
