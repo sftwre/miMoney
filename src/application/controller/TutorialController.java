724fd7ab989a28884219e4a5cd394e00a697966b
kel <kel@DESKTOP-CP95OC2.attlocal.net>
2017-11-12 18:22:34 -0600

    updated controller so far

--- src/application/controller/TutorialController.java
+++ src/application/controller/TutorialController.java
@@ -1,7 +1,10 @@
 package application.controller;
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 
 public class TutorialController {
@@ -11,7 +14,12 @@ public class TutorialController {
 
     @FXML
     void nextScene(ActionEvent event) {
-    	
+    	Parent tutor;
+    	try {
+    		tutor = FXMLLoader.load(getClass().getClassLoader().getResource("application.view.resources/Tutorial2.0.fxml"));
+    	}catch(IOException e) {
+    		e.printStackTrace();
+    	}
 
     }
     @FXML
