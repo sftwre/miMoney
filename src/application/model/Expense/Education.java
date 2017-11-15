c751d7f0ee3fa06ac02aea064406f80f152eeea4
Isaac Buitrago <mightyking7@gmail.com>
2017-11-15 06:06:02 -0600

    Added two new Expense calsses

---
+++ src/application/model/Expense/Education.java
@@ -0,0 +1,17 @@
+package application.model.Expense;
+
+import application.model.Date;
+
+/**
+ * 
+ * @author Isaac Buitrago
+ *
+ */
+public class Education extends Expense implements VariableExpense {
+
+	public Education(double amount, Date date, String item) {
+		super(amount, date, item);
+		
+	}
+
+}
---
+++ src/application/model/Expense/Utilities.java
@@ -0,0 +1,17 @@
+package application.model.Expense;
+
+import application.model.Date;
+
+/**
+ * 
+ * @author Isaac Buitrago
+ *
+ */
+public class Utilities extends Expense implements FixedExpense {
+
+	public Utilities(double amount, Date date, String item) {
+		super(amount, date, item);
+		
+	}
+
+}
