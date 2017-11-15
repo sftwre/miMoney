9b2cc1b1f56819589ed4c242ed0b93158fe68937
Isaac Buitrago <mightyking7@gmail.com>
2017-11-15 06:56:25 -0600

    Added Subscriptions Expense class

---
+++ src/application/model/Expense/Subscriptions.java
@@ -0,0 +1,19 @@
+package application.model.Expense;
+
+import application.model.Date;
+
+/**
+ * 
+ * @author Isaac Buitrago
+ *
+ */
+
+public class Subscriptions extends Expense implements VariableExpense {
+
+	public Subscriptions(double amount, Date date, String item) {
+		
+		super(amount, date, item);
+		
+	}
+
+}
