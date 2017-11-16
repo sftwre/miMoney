23056d120771fc476b52267aa18a987ee6b2aa15
kel <kel@172.24.64.142>
2017-11-16 13:32:29 -0600

    controller update

---
+++ src/application/controller/CreateAccountController.java
@@ -0,0 +1,22 @@
+package application.controller;
+
+
+
+
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CreateAccountController {
+
+    @FXML
+    private Button createAcctButton;
+
+    @FXML
+    void createAcct(ActionEvent event) {
+
+    }
+
+}
