f0dcb1cf35e593f9d2aa8197de82b063eed7f956
kel <kel@172.24.64.142>
2017-11-16 14:18:02 -0600

    tutorial updated

--- src/application/controller/TutorialController.java
+++ src/application/controller/TutorialController.java
@@ -13,26 +13,44 @@ import javafx.scene.control.Button;
 
 public class TutorialController {
 
+	String scene = "Tutorial";
+	int sceneCnt = 0;
     @FXML
     private Button nextSceneButton;
 
     @FXML
     void nextScene(ActionEvent event) {
-    	//Parent tutor;
+   
     	try {
     		Parent tutor;
-    		tutor = FXMLLoader.load(getClass().getClassLoader().getResource("/Tutorial2.0.fxml"));
+    		sceneCnt++;
+    		
+    		tutor = FXMLLoader.load(getClass().getClassLoader().getResource("/" + scene + "" + sceneCnt +".fxml"));
+    		
     		
  
     		Main.stage.setScene(new Scene(tutor, 500, 575));
     		Main.stage.show();
+    		//Math.ceil();
     	}catch(IOException e) {
     		e.printStackTrace();
+    		System.err.println("Tutorial page not found!");
     	}
 
     }
     @FXML
     void getStarted(ActionEvent event) {
+    	
+    	try {
+    		Parent start;
+        	start = FXMLLoader.load(getClass().getClassLoader().getResource("/MainView.fxml"));
+        	Main.stage.setScene(new Scene(start, 500, 575));
+    	}catch(IOException x) {
+    		x.printStackTrace();
+    	}
+    	
+    	
+    	
 
     }
 
