7bc8f0a5825d431b666e81792ffccbd31ed1503c
Jonathan <jguzman180@outlook.com>
2017-11-16 11:43:41 -0600

    CalendarController.java and associated fxml's updated

--- src/application/controller/CalendarController.java
+++ src/application/controller/CalendarController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -16,6 +17,7 @@ import java.time.YearMonth;
 import java.util.ArrayList;
 
 import application.Main;
+import application.view.CalendarView;
 
 /**
  * @author Jonathan Remote
@@ -30,17 +32,30 @@ public class CalendarController {
 	@FXML
 	private GridPane monthTile;
 	
-	//private ArrayList<> displayedDays = new ArrayList<>();
+	private CalendarView view;
+	
+	private ArrayList<PaneNode> list;
+
+	private YearMonth currentYearMonth;
+
 	
 	public void initialize() {
-monthTile.setPrefSize(640, 380);
+		monthTile.setPrefSize(420, 380);
+		monthTile.setMinSize(420, 380);
+		view = new CalendarView();
+		list = new ArrayList<PaneNode>(42);
 		
-		for(int i = 0; i<6; i++)
+		for(int i = 0; i<7; i++)
 		{
-			for(int j = 0; j<7; j++) {
-				PaneNode specificDay = new PaneNode();
-				specificDay.setPrefSize(50, 40);
-				monthTile.add(specificDay, j, i);
+			//TODO: Set day of the week here
+			for(int j = 0; j<6; j++) {
+				/*PaneNode specificDay = new PaneNode();
+				specificDay.setPrefSize(75, 70);
+				specificDay.setDate(LocalDate.now());
+		        LocalDate calendarDate = LocalDate.of(currentYearMonth.getYear(), currentYearMonth.getMonthValue(), 1);*/
+				monthTile.add(new Label("myStr"), i, j);
+
+				//list.add(specificDay);
 			}
 		}// END forLoop adding specificDay's to monthTile
 		
--- src/application/view/CalendarView.java
+++ src/application/view/CalendarView.java
@@ -3,6 +3,13 @@
  */
 package application.view;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.ArrayList;
+
+import application.controller.PaneNode;
+import javafx.scene.text.Text;
+
 /**
  * @author Jonathan
  *
@@ -10,15 +17,15 @@ package application.view;
 public class CalendarView {
 	
 	public CalendarView() {
-		
+
 	}// END constructor
 	
 	/**
 	 * This method fills the current month tiles days
 	 * with the corresponding days for the month.
 	 */
-	public void fill() {
-		
+	public void fill(YearMonth yearMonth) {
+
 	}// END fill()
 
 }
