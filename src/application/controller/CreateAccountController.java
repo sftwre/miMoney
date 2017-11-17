aef1f303595c81e85bebf0661917319a0b823d58
kel <kel@172.24.67.181>
2017-11-17 14:58:50 -0600

    create account updates

--- src/application/controller/CreateAccountController.java
+++ src/application/controller/CreateAccountController.java
@@ -8,14 +8,48 @@ package application.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class CreateAccountController {
 
     @FXML
     private Button createAcctButton;
+    VBox fields = new VBox();
+    Text usernameTxt = new Text("Username:");
+    TextField user_name = new TextField();
+    Text phoneNmTxt = new Text("Phone Number:");
+    TextField phone_number = new TextField();
+    Text passwordTxt = new Text("Password:");
+    TextField password = new TextField();
+    Text confirmPassTxt = new Text("Confirm Password:");
+    TextField confirmPass = new TextField();
+    Text incomeTxt = new Text("Income:");
+    TextField income = new TextField();
+    Text fianancialTxt = new Text("Fianancial Information:");
+    TextField fiananceInfo = new TextField();
 
     @FXML
     void createAcct(ActionEvent event) {
+    	if(user_name.getText().trim().isEmpty()) {
+    		System.err.println("You must enter your username!");
+    	}
+    	if(password.getText().trim().isEmpty()) {
+    		System.err.println("You must enter a password!");
+    	}
+    	if(confirmPass.getText().trim().isEmpty()) {
+    		System.err.println("You must confirm your password!");
+    	}
+    	if(phone_number.getText().trim().isEmpty()) {
+    		System.err.println("You must enter your phone number!");
+    	}
+    	if(income.getText().trim().isEmpty()) {
+    		System.err.println("You must enter your monthly income!");
+    	}
+    	if(fiananceInfo.getText().trim().isEmpty()) {
+    		System.err.println("You must enter your fianancial information!");
+    	}
 
     }
 
