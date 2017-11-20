3ae6ddc3c591290a863fb266826564542ca301bd
Jonathan <jguzman180@outlook.com>
2017-11-20 08:33:42 -0600

    LoginController.java loginButton modified, should now work

--- src/application/controller/LoginController.java
+++ src/application/controller/LoginController.java
@@ -10,13 +10,14 @@ import application.model.SecureOldPass;
 import application.model.SecureOldUser;
 import application.model.User;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
-import javafx.scene.control.HyperlinkBuilder;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Modality;
@@ -46,25 +47,41 @@ public class LoginController {
 	@FXML
 	Button loginButton;
 	
+	@FXML
+	Label incorrectCombo;
+	
 	private User currentUser = new User("testUser77");	//in the mean time, John
 	private SecureOldPass oldPass;
 	private SecureOldUser oldUser;
+	private ActionEvent e;
 	
 	public void initialize() {
 		oldUser = new SecureOldUser();
-		currentUser.setUserAuthenticated(oldUser.auth(userField.getCharacters().toString()));
 		oldPass = new SecureOldPass();
-		currentUser.setPassAuthenticated(oldPass.secure(currentUser.getUsername(), passField.getCharacters()));
-		
-		//if(currentUser.isPassAuthenticated() && currentUser.isUserAuthenticated())
-			//TODO: Login button goes to FinancialOverview.fxml
-		
-		//TODO: Login button says "Wrong username/password"
-	}// END initialize()
 
+		currentUser.setUserAuthenticated(oldUser.auth(userField.getCharacters().toString()));
+		
+			if(!currentUser.isUserAuthenticated())
+				currentUser.setPassAuthenticated(false);
+			else if(currentUser.isUserAuthenticated())
+				currentUser.setPassAuthenticated(oldPass.secure(currentUser.getUsername(), passField.getCharacters()));
+
+	loginButton.setOnAction(new EventHandler<ActionEvent>() {
+		@Override
+		public void handle(ActionEvent e) {
+			if(currentUser.isPassAuthenticated())
+			{
+				goToMainView(new ActionEvent());
+			}
+			incorrectCombo.setText("Username or password is incorrect.");
+		}
+	});
+		
+	}// END initialize()
+	
 	//TODO: method on loginButton press, it current user authenticated then go to mainview else stay
-	/*@FXML
-	public void addAnExpense(ActionEvent event) {
+	@FXML
+	public void goToMainView(ActionEvent event) {
 		Stage popUp =  new Stage();
 		
 		popUp.initModality(Modality.APPLICATION_MODAL);
@@ -72,13 +89,13 @@ public class LoginController {
 		
 		try
 		{
-		Parent root = FXMLLoader.load(getClass().getResource("../view/resources/DatalistView.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("../view/resources/FinancialOverview.fxml"));
 		Scene scene = new Scene(root);
 		popUp.setScene(scene);
 		popUp.show();
 		} catch(IOException e){
-			System.out.printf("The resource 'view/resources/DatalistView.fxml' could not be located");
+			System.out.printf("The resource 'view/resources/FinancialOverview.fxml' could not be located");
 		}// END try/catch load FXML
-	}*/
+	}
 	
 }//END CONTROLLER CLASS LoginController
