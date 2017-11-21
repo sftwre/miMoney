2a78372eae4561a85a4e5f25c2d43be8e5d1729c
SamDash237 <31454024+SamDash237@users.noreply.github.com>
2017-11-21 10:50:45 -0600

    Added parameters to constructor

--- src/application/controller/SavingsCalculatorController.java
+++ src/application/controller/SavingsCalculatorController.java
@@ -1,6 +1,7 @@
 package application.controller;
 
 import java.awt.TextField;
+
 import application.model.SavingsCalculatorModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -24,10 +25,10 @@ public class SavingsCalculatorController{
     @FXML
     private TextField time;
     
-    private SavingsCalculatorModel model = new SavingsCalculatorModel();
     private boolean begin = true;
     private double num1 = 0;
     private double num2 = 0;
+    private SavingsCalculatorModel model = new SavingsCalculatorModel(num1, num2);
     
     /**
      * Do computations here, call the handle methods above if you have to
@@ -46,7 +47,7 @@ public class SavingsCalculatorController{
     	
     	String val = ((Button)event.getSource()).getText();
     	
-    	if (!"Calculate".equals(val)) {
+    	if (val.equals("Calculate")) {
 		    	if(time.getText().equals(""))
 		    		return;
     	num1 = Double.parseDouble(g);
