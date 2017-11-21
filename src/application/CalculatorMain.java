400d38df91b537f923c833bd9d85ac1cc6d9c808
SamDash237 <31454024+SamDash237@users.noreply.github.com>
2017-11-21 10:57:42 -0600

    Main class for testing Savings Calculator

---
+++ src/application/CalculatorMain.java
@@ -0,0 +1,31 @@
+package application.main;
+
+import javafx.application.Application;  
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class CalculatorMain extends Application{
+
+	public static Stage stage;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+		
+			Parent root = FXMLLoader.load(getClass().getResource("/CalcView.fxml"));
+			primaryStage.setScene(new Scene(root, 400, 400));
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		CalculatorMain.stage = primaryStage;
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
