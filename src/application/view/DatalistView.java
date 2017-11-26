49e196212a0caea4b7025f6e9289587fe506e15b
Jonathan Guzman <jguzman180@outlook.com>
2017-11-25 21:43:13 -0600

    Updating some DataList stuff for consistency

---
+++ src/application/view/DatalistView.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package application.view;
+
+/**
+ * @author jguzm
+ *
+ */
+public class DatalistView {
+	
+	public void fill() {
+		
+	}//END fill
+	
+	public void addNewSection() {
+		
+	}
+
+}
