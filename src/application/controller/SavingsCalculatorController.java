5ca44b7d5827d87033012c451e193559123d7f3a
samda <samda@DESKTOP-31EGA3A.attlocal.net>
2017-11-26 18:14:36 -0600

    Removed superfluous code from SavingsCalculatorController handle

--- src/application/controller/SavingsCalculatorController.java
+++ src/application/controller/SavingsCalculatorController.java
@@ -1,11 +1,9 @@
 package application.controller;
 
 import java.awt.TextField;
-
 import application.model.SavingsCalculatorModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 
 
 /**
@@ -25,7 +23,6 @@ public class SavingsCalculatorController{
     @FXML
     private TextField time;
     
-    private boolean begin = true;
     private double num1 = 0;
     private double num2 = 0;
     private SavingsCalculatorModel model = new SavingsCalculatorModel(num1, num2);
@@ -39,23 +36,10 @@ public class SavingsCalculatorController{
     {
     	String g = goal.getText();
     	String d = deposit.getText();
-    	
-    	if (begin) {
-            time.setText("");
-            begin = false;
-        }
-    	
-    	String val = ((Button)event.getSource()).getText();
-    	
-    	if (val.equals("Calculate")) {
-		    	if(time.getText().equals(""))
-		    		return;
     	num1 = Double.parseDouble(g);
     	num2 = Double.parseDouble(d);
-    	}
-    	else {
+
     	time.setText(String.valueOf(model.savingsTime(num1,num2)));
-    	begin = true;
+    	System.out.println("clicked!");
     	}
-    }
 }
