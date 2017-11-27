0c18a5d05c2febe49f063474fd15b39fb2c99972
Isaac Buitrago <mightyking7@gmail.com>
2017-11-26 20:04:00 -0600

    Added Delete Budget Item and add Budget Item

--- src/application/controller/CreateBudgetController.java
+++ src/application/controller/CreateBudgetController.java
@@ -1,15 +1,25 @@
 package application.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.FontSmoothingType;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 
 /**
@@ -20,12 +30,20 @@ import javafx.scene.layout.StackPane;
  */
 public class CreateBudgetController {
 
+	@FXML
+    private Text gasText;
+	
+	@FXML
+    private Text foodText;
+	
+	@FXML
+    private Text apperalText;
+	
     @FXML
     private Button deleteButton;
 
     @FXML
     private TextField foodTextField;
-
 
     @FXML
     private TextField gasTextField;
@@ -37,7 +55,7 @@ public class CreateBudgetController {
     private CheckBox gasExpense;
 
     @FXML
-    private CheckBox rentExpense;
+    private CheckBox apperalExpense;
 
     @FXML
     private Button clearButton;
@@ -46,7 +64,7 @@ public class CreateBudgetController {
     private Button continueButton;
 
     @FXML
-    private TextField rentTextField;
+    private TextField apperalTextField;
 
     @FXML
     private CheckBox foodExpense;
@@ -58,14 +76,24 @@ public class CreateBudgetController {
     private StackPane buttonStackPane;
     
     // Options to display adding a new Expense category
+    private ObservableList<String> expenseOptions = FXCollections.observableArrayList();
     
-    private ObservableList<String> expenseOptions = FXCollections.observableArrayList();	
+    // Queue of indexes to delete from a GridPane
+    private LinkedList<Integer> indexesToDelete = new LinkedList<Integer>();
+    
+    // index to place new Budget Items in budgetGridPane
+    private int rowIndex;	
+    
+    // Delete and Clear Buttons in Stack Pane
+    private ObservableList<Node> stackPaneButtons;
+    
+    // List of Check Boxes in budgetGridPane
+    private ArrayList<CheckBox> checkBoxes = new ArrayList<CheckBox>();
 
     
     /**
      * Used to initialize components of the view
      */
-    
     public void initialize()
     {
     	Thread comboBoxThread = new Thread(){
@@ -80,22 +108,128 @@ public class CreateBudgetController {
     	};
     	
     	comboBoxThread.run();
+    	
+    	 stackPaneButtons = buttonStackPane.getChildren();
+    	 
+    	 //add the Check Boxes to the Collection of CheckBoxes
+    	 checkBoxes.add(foodExpense);
+    	 checkBoxes.add(gasExpense);
+    	 checkBoxes.add(apperalExpense);
+    	 
+    	 // set the rowIndex to last row in budgetGridPane
+    	 rowIndex =  budgetGridPane.getRowConstraints().size()-1;
+    	 
     }
 
+    
+    /**
+     * Used to display the Delete Button to delete a budget item from the Budget
+     * and return the row index of the Check Box selected.
+     * @param event that called this method
+     */
     @FXML
-    void deleteGoal(ActionEvent event) 
+   public void budgetItemSelected(ActionEvent event) 
     {
+    	
+    	Integer index = GridPane.getRowIndex(((CheckBox)event.getSource())); // row index of Check Box that was just clicked
+    	
+    	boolean budgetItemToEdit = budgetItemToEdit();					 	// flag to determine if a Budget Item is selected for deletion
+    	
+    	// Get the row index in the GridPane of the selected CheckBox
+    	if(((CheckBox)event.getSource()).isSelected())
+    		indexesToDelete.add(index);
+    	
+    	// the CheckBox was unselected and should not be deleted
+    	else
+    		indexesToDelete.remove(index);
+    		
+    	// Bring Delete Button forward in the StackPane if the Top Button is the "Clear" Button
+    	if(((Button)stackPaneButtons.get(1)).getText().equals("Clear") && budgetItemToEdit)
+    		moveButtonInStackPane(this.buttonStackPane);
+    	
+    	// If there are no Items selected, bring the Clear Button forward
+    	else if( ! budgetItemToEdit)
+    		moveButtonInStackPane(this.buttonStackPane);
+
+    }
+    
+    /**
+     * Used to delete all Budget items that had a CheckBox
+     * selected and push the Delete Button back in the StackPane.
+     * @param event
+     */
+    @FXML
+    public void deleteGoal(ActionEvent event) 
+    {
+    	ArrayList<Node> nodesToDelete = new ArrayList<Node>();
+    	
+    	for(Node child : budgetGridPane.getChildren())
+    	{
+    		if(indexesToDelete.contains(GridPane.getRowIndex(child)))
+    		{
+    			nodesToDelete.add(child);
+    		}
+    			
+    	}
+    	
+    	budgetGridPane.getChildren().removeAll(nodesToDelete);
+    	
+    	moveButtonInStackPane(this.buttonStackPane);
+    }
+
+    
+    @FXML
+    public void clearTextFields(ActionEvent event) {
 
     }
 
+    /**
+     * Used to add a new Budget Item to the Budget.
+     * Each Item contains a Label, TextField, and CheckBox
+     * @param event
+     */
     @FXML
-    void clearTextFields(ActionEvent event) {
-
-    }
-
-    @FXML
-    void showExpenseCategories(ActionEvent event) {
-
+    public void addExpenseCategories(ActionEvent event) 
+    {
+    	Text itemCategory;		// Expense Category of the Budget Item
+    	TextField itemAmount;	// Amount to allocate for the Expense Category
+    	CheckBox itemSelected;	// CheckBox for selecting and deleting a Budget Item
+    	
+    	itemCategory = new Text((String)((ComboBox)(event.getSource())).getValue());
+    	
+    	itemAmount = new TextField();
+    	
+    	itemSelected = new CheckBox("Select");
+    	
+    	// set the properties of the Text
+    	itemCategory.setWrappingWidth(200);
+    	itemCategory.setTextAlignment(TextAlignment.CENTER);
+    	itemCategory.setFontSmoothingType(FontSmoothingType.GRAY);
+    	
+    	// register an event for the CheckBox
+    	itemSelected.addEventHandler(ActionEvent.ACTION, this::budgetItemSelected);
+    	
+    	try
+    	{
+    		
+    	// place a new Budget Item in empty rows first
+    	if(indexesToDelete.isEmpty())
+    		rowIndex++;
+    	
+    	else
+    		rowIndex = indexesToDelete.remove();
+    	
+    	budgetGridPane.addRow(rowIndex, itemCategory, itemAmount, itemSelected);
+    	
+    	// align the CheckBox and TextField
+    	GridPane.setValignment(itemAmount, VPos.CENTER);
+    	GridPane.setHalignment(itemSelected, HPos.CENTER);
+    	
+    	} catch(Exception e)
+    	{
+    	System.out.println(rowIndex);
+    	}
+    	
     }
 
     
@@ -114,7 +248,41 @@ public class CreateBudgetController {
     }
     
     @FXML
-    void Continue(ActionEvent event) {
+    public void Continue(ActionEvent event) {
 
     }
+    
+    /**
+     * Used to move buttons in the buttonStackPane forward and backward
+     * 
+     */
+    private void moveButtonInStackPane(StackPane stackPane)
+    {
+    	ObservableList<Node> nodes = stackPane.getChildren();
+    	
+    	if(nodes.size() > 1 )
+    	{
+    		Node top = nodes.get(nodes.size() - 1);
+    		
+    		top.toBack();
+    	}
+    }
+    
+    /**
+     * Used to determine if a Budget Item is selected for deletion
+     * @return true if at least one Check Box is selected, false otherwise
+     */
+    private boolean budgetItemToEdit()
+    {
+    	boolean itemSelected = false;
+    	
+    	for(CheckBox box : checkBoxes)
+    	{
+    		if( box.isSelected())
+    			itemSelected = true;
+    	}
+    	
+    	return(itemSelected);
+    }
+    
 }
