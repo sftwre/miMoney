59163293b6540d413e55c2e8b082f0f9de559d19
eeo072 <eeo072@N0000A087.cs.utsarr.net>
2017-11-28 10:16:11 -0600

    CalculatorController compiles

--- src/application/controller/CalculatorController.java
+++ src/application/controller/CalculatorController.java
@@ -1,17 +1,45 @@
 package application.controller;
 
+import javafx.scene.control.TextField;
+import application.model.SavingsCalculatorModel;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+
 
 /**
  * 
  * @author Sam Dash
  *
  */
-public class CalculatorController implements EventHandler<ActionEvent> {
 
-	@Override
-	public void handle(ActionEvent event) {
-		System.out.println("clicked!");
-	}
+public class CalculatorController implements EventHandler<ActionEvent>{
+	
+    @FXML
+    private TextField goal;
+    
+    @FXML
+    private TextField deposit;
+    
+    @FXML
+    private TextField time;
+    
+    private double num1 = 0;
+    private double num2 = 0;
+    private SavingsCalculatorModel model = new SavingsCalculatorModel(num1, num2);
+    
+    /**
+     * Do computations here, call the handle methods above if you have to
+     * @param event
+     */
+    @Override
+    public void handle(ActionEvent event)
+    {
+    	String g = goal.getText();
+    	String d = deposit.getText();
+    	num1 = Double.parseDouble(g);
+    	num2 = Double.parseDouble(d);
+    	time.setText(String.valueOf(model.savingsTime(num1,num2)));
+    	System.out.println("clicked!");
+    	}
 }
