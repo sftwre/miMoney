f98f4ca2d897e569bc5b514ec5e24e137e60fe6e
Jonathan <jguzman180@outlook.com>
2017-11-28 11:44:19 -0600

    Removed redundancies in DatalistView

--- src/application/view/DatalistView.java
+++ src/application/view/DatalistView.java
@@ -3,12 +3,15 @@
  */
 package application.view;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
+
 /**
  * @author jguzm
  *
  */
 public class DatalistView {
-	
+
 	public void fill() {
 		
 	}//END fill
