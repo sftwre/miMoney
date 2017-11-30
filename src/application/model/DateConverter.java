477ee8e84a6054583b2dff7df9cf5f0070d66cff
Jonathan <jguzman180@outlook.com>
2017-11-30 12:41:05 -0600

    Created the DateConverter class. Use to convert LocalDate to Date

---
+++ src/application/model/DateConverter.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package application.model;
+
+import java.time.LocalDate;
+
+/**
+ * @author Jonathan
+ *
+ */
+public class DateConverter {
+	
+	/**
+	 * convertDate(LocalDate date) takes in a local date and converts it to the
+	 * Date type used in all of the expense classes (and other places too).
+	 * @param date
+	 * 				The LocalDate type given
+	 * @return
+	 * 				The method returns a Date type of the given date
+	 */
+		
+	public static Date convertDate(LocalDate date) {
+		Date d;
+		d = new Date(date.getMonthValue(), date.getDayOfMonth(), date.getYear());
+
+		return d;
+	}// END convertDate
+
+}
