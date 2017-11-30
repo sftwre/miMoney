ad179ed09cf2ecfe14cef57596d17d3afe73e0e1
Jonathan <jguzman180@outlook.com>
2017-11-30 09:58:29 -0600

    added the comments needed in MonthFormatter.java

--- src/application/model/MonthFormatter.java
+++ src/application/model/MonthFormatter.java
@@ -11,6 +11,26 @@ import java.time.Month;
  */
 public class MonthFormatter {
 	
+	
+	/**
+	 * formatMonth(Month mo) takes in a Month type. The month type is returned from the
+	 * YearMonth method getMonth(). Use this method to format the given month and return
+	 * it formatted properly.
+	 * 
+	 * e.g.
+	 * YearMonth currentMonth = YearMonth.now() //this gets the current month and year
+	 * System.out.printf("%s", currentMonth.MonthFormatter.formatMonth());
+	 * Output: November
+	 * 
+	 * If you simply print currentMonth.getMonth().toString() you will get the current
+	 * month printed in all CAPS.
+	 * 
+	 * @param mo
+	 * 			The month type given
+	 * @return
+	 * 		Returns a formatted month with the first letter capitalized and the rest lowercase
+	 * 				e.g. November vs NOVEMBER (NOVEMBER is printed when using a Month types toString()
+	 */
 	public static String formatMonth(Month mo){
 	 return mo.toString().substring(0, 1).toUpperCase()
 		+ mo.toString().substring(1).toLowerCase();
