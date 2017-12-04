70489f666a9971eb540b794b2aa02367ac6d7ab2
Isaac Buitrago <mightyking7@gmail.com>
2017-12-04 00:22:53 -0600

    added methods to support more robust tracking of spending

--- src/application/model/ExpenseTracker.java
+++ src/application/model/ExpenseTracker.java
@@ -1,6 +1,7 @@
 package application.model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import application.model.Expense.Expense;
 
@@ -17,16 +18,38 @@ public class ExpenseTracker
 {
 	
 	/**
-	 * Living expenses include Housing, Auto expenses, and General Insurance payments
+	 * Living expenses include variable Expenses 
+	 * that cannot be accounted for by the user.
 	 */
 	private ArrayList<Expense> livingExpenses;
 	
+	/**
+	 * Date for which to track Expenses 
+	 */
+	private Date date;
+	
+	
+	/**
+	 * Constructor that does not take a date
+	 */
 	public ExpenseTracker() 
+	{
+		livingExpenses = new ArrayList<Expense>();	
+	}
+	
+	
+	/**
+	 * Constructor that takes a Date
+	 * @param date
+	 */
+	public ExpenseTracker(Date date) 
 	{
 
 		livingExpenses = new ArrayList<Expense>();
+		this.date = date;
 	}
 	
+
 	/**
 	 *@return ArrayList of expenses for the month 
 	 */
@@ -34,6 +57,25 @@ public class ExpenseTracker
 	{
 		
 		return this.livingExpenses.get(index);
+	}
+	
+	/**
+	 * Used to return all Expenses currently being tracked
+	 * @return 
+	 */
+	public ArrayList<Expense> getExpenses()
+	{
+		return(this.livingExpenses);
+	}
+	
+	public Date getDate() 
+	{
+		return date;
+	}
+
+	public void setDate(Date date) 
+	{
+		this.date = date;
 	}
 	
 	/**
@@ -47,6 +89,14 @@ public class ExpenseTracker
 	}
 	
 	/**
+	 * Used to add a List of Expense items to the tracker
+	 */
+	public void addExpenses(List<Expense> expenses)
+	{
+		this.livingExpenses.addAll(expenses);
+	}
+	
+	/**
 	 * This function removes an expense from the list.
 	 * @param expense to be deleted from the list
 	 */
@@ -56,6 +106,26 @@ public class ExpenseTracker
 		
 		this.livingExpenses.remove(expense);
 		
+	}
+	
+	/**
+	 * Used to return the total amount spent for a specific Expense category
+	 * in the current month.
+	 * @return the total amount spent in an Expense category
+	 */
+	public double totalSpentInCategory(Expense expenseCategory)
+	{
+		double amountSpent = 0;
+		
+		String category = expenseCategory.getClassName();
+		
+		for(Expense e : livingExpenses)
+		{
+			if(e.getClassName().equals(category))
+				amountSpent += e.getAmmount();		
+		}
+		
+		return amountSpent;
 	}
 	
 
